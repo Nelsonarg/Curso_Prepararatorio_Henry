48d00b79798311f4bf98d6b89654c4d02669790a
Nelsonarg <silvanelson.arg@gmail.com>
2023-06-16 10:22:17 -0300

    first commit

---
+++ Operadores.js
@@ -0,0 +1,27 @@
+
+console.log(14 -7);
+console.log(14 /7);
+console.log(14 * 7);
+console.log(14 +7);
+
+// Se ejecuta primero la multiplicacion
+
+console.log(3+5*2-8);
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
---
+++ Variables.js
@@ -0,0 +1,23 @@
+
+
+var cajon1 = "guantes verdes";
+
+
+// String
+var bootcamp = 'Henry';
+
+//Numeros
+var numerosEnteros = 17;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+//Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefine
+var cajon1;
+console.log(cajon1);
+
+//Null
+var sinNada = null;
